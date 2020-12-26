2c4c328cd91811791b6091714c9916d6e02ce7b2
paulo-789 <jorhnepaulone@gmail.com>
2020-12-26 20:43:33 +0300

    finished with the api

--- js/scripts.js
+++ js/scripts.js
@@ -17,12 +17,58 @@ function getMovies(searchText){
         <div class="well text-center">
         <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}">
         <h5>${movie.title}</h5>
-        <a onclick="movieSelected('${movie.id}')" class="btn btn-primary" href="#">Movie Details</a>
+        <a onclick="movieSelected('${movie.id}')" class="btn btn-info" href="#">Movie Details</a>
         </div>
         </div>
         `;
     }); 
     $('#movies').html(output);
+    })
+    .catch((err) => {
+       console.log(err);
+    });
+}
+function movieSelected(id){
+    sessionStorage.setItem('movieId', id);
+    window.location = 'movie.html';
+    return false;
+}
+function getMovie(){
+    let movieId = sessionStorage.getItem('movieId');
+    axios.get("https://api.themoviedb.org/3/movie/" + movieId +"?api_key=f978469236288b7c33c1b9f16b70da07")
+    .then(function(response){
+        // console.log(response);
+        let movie = response.data;
+
+        let output =`
+        <div class = 'row'>
+         <div class="col-md-4">
+         <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="thumbnail">
+         </div>
+         <div class="col-md-8">
+         <h2>${movie.title}<h2>
+         <ul class="list-group">
+         <li class="list-group-item"><strong>Genre;</strong>${movie.genres[0].name}, ${movie.genres[1].name}</li>
+         <li class="list-group-item"><strong>Released;</strong>${movie.release_date}</li>
+         <li class="list-group-item"><strong>Rated;</strong>${movie.vote_average}</li>
+         <li class="list-group-item"><strong>Runtime;</strong>${movie.runtime}</li>
+         <li class="list-group-item"><strong>Production Companies:</strong> ${movie.production_companies[0].name} min.</li>
+         </ul>
+         </div>
+        </div>
+        <div class="row">
+        <div class="well">
+          <h3>Plot</h3>
+          ${movie.overview}
+          <hr>
+
+          <a href="index.html" class="btn btn-success">Go Back To Search</a>
+        </div>
+      </div>
+        `;
+
+        $('#movie').html(output);
+   
     })
     .catch((err) => {
        console.log(err);
